ef45c923e6cf145de93ff96121fde6564ce402f1
mitsu <kdmtsr0130@gmail.com>
2025-08-11 09:32:51 +0900

    test pr_comment

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestEvenOrOdd(t *testing.T) {
+func TestEvenOrOdd3(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "even" {
 		t.Errorf("expected: even, actual: %s", result)
